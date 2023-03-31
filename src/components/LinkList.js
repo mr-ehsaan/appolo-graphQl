40e4a515e022c1c62f7e8f3dacace2d0747502a4
mr-ehsan <99403938+mr-ehsaan@users.noreply.github.com>
2023-03-31 16:27:21 +0500

    feat:

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Link from './Link';
-import { useQuery, gql } from '@apollo/client';
+import React from "react";
+import Link from "./Link";
+import { useQuery, gql } from "@apollo/client";
 
 const FEED_QUERY = gql`
   {
@@ -11,25 +11,33 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
       }
     }
   }
-`
-;
+`;
 const LinkList = () => {
-    const { data } = useQuery(FEED_QUERY);
-  
-    return (
-      <div>
-        {data && (
-          <>
-            {data.feed.links.map((link) => (
-              <Link key={link.id} link={link} />
-            ))}
-          </>
-        )}
-      </div>
-    );
-  };
+  const { data } = useQuery(FEED_QUERY);
+
+  return (
+    <div>
+      {data && (
+        <>
+          {data.feed.links.map((link, index) => (
+            <Link key={link.id} link={link} index={index} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default LinkList;
